22ca4c92670d811ef23a9b9cabf84103023d4fe6
Kailash Sharma <kailashps.1011@gmail.com>
2023-12-07 05:57:23 +0530

    Initial

--- app/src/main/java/ai/travel/app/navigation/NavController.kt
+++ app/src/main/java/ai/travel/app/navigation/NavController.kt
@@ -4,6 +4,7 @@ import ai.travel.app.home.ui.HomeScreen
 import ai.travel.app.home.HomeViewModel
 import ai.travel.app.home.ui.PfScreen
 import ai.travel.app.home.ui.RtScreen
+import ai.travel.app.tripDetails.TripDetailsScreen
 import androidx.compose.runtime.Composable
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
@@ -17,7 +18,7 @@ fun NavController(navHostController: NavHostController) {
 
     NavHost(navController = navHostController, startDestination = Screens.Home.route) {
         composable(Screens.Home.route) {
-            HomeScreen(viewModel = homeViewModel)
+            TripDetailsScreen(viewModel = homeViewModel)
         }
         composable(Screens.Profile.route) {
             PfScreen()
