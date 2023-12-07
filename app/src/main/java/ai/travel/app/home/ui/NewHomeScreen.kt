18b84a3450e5cc38f069b3ec59fe18fb35382c4d
Kailash Sharma <kailashps.1011@gmail.com>
2023-12-07 17:28:43 +0530

    Add PersonalRoutes

--- app/src/main/java/ai/travel/app/home/ui/NewHomeScreen.kt
+++ app/src/main/java/ai/travel/app/home/ui/NewHomeScreen.kt
@@ -1,6 +1,5 @@
 package ai.travel.app.home.ui
 
-import ai.travel.app.bottomBar.BottomBar
 import ai.travel.app.home.HomeViewModel
 import ai.travel.app.home.base64ToByteArray
 import ai.travel.app.ui.theme.appGradient
@@ -9,7 +8,6 @@ import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.LocalOverscrollConfiguration
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
@@ -24,27 +22,20 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material3.DrawerValue
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.derivedStateOf
-import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableFloatStateOf
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.navigation.compose.rememberNavController
 
 @OptIn(ExperimentalMaterial3Api::class, ExperimentalFoundationApi::class)
 @Composable
@@ -91,10 +82,17 @@ fun NewHomeScreen(viewModel: HomeViewModel, bottomBarPadding: PaddingValues) {
                         .background(appGradient),
                     state = listState
                 ) {
+
                     item {
                         ExpandedTopBarHomeScreen(
                             imageUrl = "https://lh3.googleusercontent.com/a/ACg8ocLRSg1ANIUVzU42MCsMSsHnHvu_MeSrh7lLkADF4zZptKg=s576-c-no",
                         )
+                    }
+
+                    item {
+                        PersonalRoutes()
+
+                        Spacer(modifier = Modifier.height(10.dp))
                     }
 
                     items(trips.value) { newData ->
