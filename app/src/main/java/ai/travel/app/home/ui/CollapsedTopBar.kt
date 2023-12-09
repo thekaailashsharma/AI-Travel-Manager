9487f9522cc14dfd307fadec59a6e17987ce78e7
HeyfromNandini <nandinidsecs122@gst.sies.edu.in>
2023-12-09 20:07:16 +0530

    MemoryBlock

--- app/src/main/java/ai/travel/app/home/ui/CollapsedTopBar.kt
+++ app/src/main/java/ai/travel/app/home/ui/CollapsedTopBar.kt
@@ -23,6 +23,7 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -57,11 +58,14 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.withStyle
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
@@ -125,3 +129,4 @@ fun CollapsedTopBarHomeScreen(
         }
     }
 }
+
